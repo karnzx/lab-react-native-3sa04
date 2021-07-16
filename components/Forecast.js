772b5ddb29701ec25611e51fd15b922f668f4061
Piravit Chenpittaya <karnzx-18762@hotmail.com>
2021-07-16 15:50:58 +0700

    dry detail code

--- components/Forecast.js
+++ components/Forecast.js
@@ -4,42 +4,26 @@ import { Text, View, StyleSheet, Image } from 'react-native';
 export default function Forecast(props) {
     return (
         <View>
-            <View style={styles.row}>
-                <Text style={styles.boldText}>Weather : </Text>
-                <Text style={styles.Text}>{props.main}</Text>
-            </View>
-            <View style={styles.row}>
-                <Text style={styles.boldText}>Description : </Text>
-                <Text style={styles.Text}>{props.description}</Text>
-            </View>
-            <View style={styles.row}>
-                <Text style={styles.boldText}>Lattitude : </Text>
-                <Text style={styles.Text}>{props.lat}</Text>
-            </View>
-            <View style={styles.row}>
-                <Text style={styles.boldText}>Longitude : </Text>
-                <Text style={styles.Text}>{props.lon}</Text>
-            </View>
-            <View style={styles.row}>
-                <Text style={styles.boldText}>Temp : </Text>
-                <Text style={styles.Text}>{props.temp}°C</Text>
-            </View>
-            <View style={styles.row}>
-                <Text style={styles.boldText}>Humidity : </Text>
-                <Text style={styles.Text}>{props.humidity}</Text>
-            </View>
-            <View style={styles.row}>
-                <Text style={styles.boldText}>Pressure : </Text>
-                <Text style={styles.Text}>{props.pressure}</Text>
-            </View>
-            <View style={styles.row}>
-                <Text style={styles.boldText}>WindSpeed : </Text>
-                <Text style={styles.Text}>{props.windSpeed}</Text>
-            </View>
+            <TextDetail title="Weather" text={props.main} />
+            <TextDetail title="Description" text={props.description} />
+            <TextDetail title="Lattitude" text={props.lat} />
+            <TextDetail title="Longitude" text={props.lon} />
+            <TextDetail title="Temp" text={props.temp} suffix='°C' />
+            <TextDetail title="Humidity" text={props.humidity} />
+            <TextDetail title="Pressure" text={props.pressure} />
+            <TextDetail title="WindSpeed" text={props.windSpeed} />
         </View >
     );
 }
 
+function TextDetail({ title, text, suffix }) {
+    return (
+        <View style={styles.row}>
+            <Text style={styles.boldText}>{title} : </Text>
+            <Text style={styles.Text}>{text} {suffix}</Text>
+        </View>
+    );
+}
 
 const styles = StyleSheet.create({
     boldText: { fontSize: 18, fontWeight: 'bold' },
